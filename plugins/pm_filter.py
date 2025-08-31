a871b38a970778522100cf926b79334d764b941d
riyabot1 <riyakumari727327@gmail.com>
2025-08-31 17:20:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2477,7 +2477,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "english_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/KingVj01")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/@Hacjer6865")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
@@ -3442,6 +3442,7 @@ async def global_filters(client, message, text=False):
                 break
     else:
         return False
+
 
 
 
